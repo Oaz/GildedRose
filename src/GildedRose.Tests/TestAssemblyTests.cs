fa24ce71af03d1652160cb55b089715d22e479c1
Oaz <github@azeau.com>
2011-02-18 21:12:39 +0100

    update 0

--- src/GildedRose.Tests/TestAssemblyTests.cs
+++ src/GildedRose.Tests/TestAssemblyTests.cs
@@ -12,5 +12,28 @@ namespace GildedRose.Tests
         {
             Assert.IsTrue(true);
         }
+
+        public bool AreEqual(Item i1, Item i2)
+        {
+          return i1.Name == i2.Name && i1.SellIn == i2.SellIn && i1.Quality == i2.Quality;
+        }
+
+        public bool AreEquivalent(IList<Item> l1, IList<Item> l2)
+        {
+          var areEquivalent = true;
+          for(var i=0; i<l1.Count; i++)
+            areEquivalent = areEquivalent && AreEqual(l1[i], l2[i]);
+          return areEquivalent;
+        }
+
+        [Test]
+        public void Update0()
+        {
+            var reference = new Program();
+            var refactored = new Program();
+            Assert.IsTrue( AreEqual(refactored.Items[0], reference.Items[0]) );
+            Assert.IsFalse( AreEqual(refactored.Items[0], reference.Items[1]) );
+            Assert.IsTrue( AreEquivalent(refactored.Items, reference.Items) );
+        }
     }
 }
