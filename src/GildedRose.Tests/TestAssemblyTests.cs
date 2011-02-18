8052f4845b49032ba1dbee161eb1a1d16940ebfc
Oaz <github@azeau.com>
2011-02-19 00:32:27 +0100

    update 11

--- src/GildedRose.Tests/TestAssemblyTests.cs
+++ src/GildedRose.Tests/TestAssemblyTests.cs
@@ -44,7 +44,7 @@ namespace GildedRose.Tests
         [Test]
         public void Updates()
         {
-           for(var u=0; u<=7; u++)
+           for(var u=0; u<=11; u++)
              Update(u);
         }
     }
