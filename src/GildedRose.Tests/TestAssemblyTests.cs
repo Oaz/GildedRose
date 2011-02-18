ed6643857d4f121deec138c318dbaaabd330733d
Oaz <github@azeau.com>
2011-02-19 00:55:08 +0100

    35 updates is enough

--- src/GildedRose.Tests/TestAssemblyTests.cs
+++ src/GildedRose.Tests/TestAssemblyTests.cs
@@ -13,18 +13,25 @@ namespace GildedRose.Tests
             Assert.IsTrue(true);
         }
 
-        public bool AreEqual(Item i1, Item i2)
+        public static bool AreEqual(Item i1, Item i2)
         {
           System.Console.WriteLine("< Name={0} SellIn={1} Quality={2}", i2.Name, i2.SellIn, i2.Quality);
           System.Console.WriteLine("> Name={0} SellIn={1} Quality={2}", i1.Name, i1.SellIn, i1.Quality);
           return i1.Name == i2.Name && i1.SellIn == i2.SellIn && i1.Quality == i2.Quality;
         }
 
-        public bool AreEquivalent(IList<Item> l1, IList<Item> l2)
+        public static bool QualityAreEqual(Item i1, Item i2)
+        {
+          System.Console.WriteLine("< Name={0} Quality={1}", i2.Name, i2.Quality);
+          System.Console.WriteLine("> Name={0} Quality={1}", i1.Name, i1.Quality);
+          return i1.Name == i2.Name && i1.Quality == i2.Quality;
+        }
+
+        public static bool AreEquivalent(IList<Item> l1, IList<Item> l2, Func<Item,Item,bool> comparer)
         {
           var areEquivalent = true;
           for(var i=0; i<l1.Count-1; i++)
-            areEquivalent = areEquivalent && AreEqual(l1[i], l2[i]);
+            areEquivalent = areEquivalent && comparer(l1[i], l2[i]);
           return areEquivalent;
         }
 
@@ -38,14 +45,29 @@ namespace GildedRose.Tests
               reference.OldUpdateQuality();
               refactored.NewUpdateQuality();
             }
-            Assert.IsTrue( AreEquivalent(refactored.Items, reference.Items) );
+            Assert.IsTrue( AreEquivalent(refactored.Items, reference.Items, TestAssemblyTests.AreEqual) );
         }
 
         [Test]
         public void Updates()
         {
-           for(var u=0; u<=30; u++)
+           for(var u=0; u<=35; u++)
              Update(u);
+        }
+
+        [Test]
+        public void ThirtyFiveUpdatesIsEnough()
+        {
+            System.Console.WriteLine("================ 35 updates is enough ===================");
+            var updateA = new Program();
+            var updateB = new Program();
+            for(var i=1; i<=34; i++)
+            {
+              updateA.OldUpdateQuality();
+              updateB.OldUpdateQuality();
+            }
+            updateB.OldUpdateQuality();
+            Assert.IsTrue( AreEquivalent(updateA.Items, updateB.Items, TestAssemblyTests.QualityAreEqual) );
         }
     }
 }
